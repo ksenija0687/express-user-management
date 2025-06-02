926174c6b8b31c7a8e8402f26eef76cd12bf1e9e
ksenija0687 <oksdjan68@gmail.com>
2025-06-02 11:27:32 +0300

    Add get method for blank page

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const express = require("express");
 const port = 3011
 const app = express();
 
+//http://localhost:3011/
 app.use(express.json());
+
+const users = [];
+
+app.get("/", (req,res) => {
+    res.status(200).send("Hello World");
+});
+
 
 app.listen(port,()=>{
     console.log(`server is running http://localhost:${port}`);
